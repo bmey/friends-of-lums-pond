67f816eab8e37aeda2e2318d29bb5a16e5a71eb4
Bryan Mey <bryan.j.mey@gmail.com>
2023-09-19 17:46:04 -0400

    add GH pages script

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://bmey.github.io',
+	base: '/friends-of-lums-pond',
 	integrations: [mdx(), sitemap()],
 });
