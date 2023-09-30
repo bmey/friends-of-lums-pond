f6f8a3206b12e05078589dc9e57bf34904fc5189
Bryan Mey <bryan.j.mey@gmail.com>
2023-09-30 13:27:40 -0400

    refactor links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Friends of Lums Pond";
+export const DONATE_URL = "https://donorbox.org/friends-of-lums-pond-state-park?default_interval=o";
+export const EMAIL = "friends-of-lums-pond-list@googlegroups.com";
+export const FACEBOOK_URL = "https://www.facebook.com/FriendsofLumsPond";
+export const JOIN_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdlFNu4wlu5ITkDM8ape6Pi6icwg4CB_ig7spxk4JjcHyHCHw/viewform?usp=sf_link";
 export const SITE_DESCRIPTION = "Welcome to Friends of Lums Pond State Park!";
+export const SITE_TITLE = "Friends of Lums Pond";
