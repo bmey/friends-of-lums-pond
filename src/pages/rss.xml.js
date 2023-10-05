d11d139760c3a231c62e6639f03b912e58645116
Bryan Mey <bryan.j.mey@gmail.com>
2023-10-05 09:03:36 -0400

    update rss feed item paths and channel image

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import { getUrl } from "../components/getUrl";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import Logo from "../images/logo.png";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -8,9 +10,14 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    customData: `<image>
+      <url>${context.site.origin + Logo.src}</url>
+      <title>${SITE_TITLE}</title>
+      <link>${context.site}</link>
+    </image>`,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: getUrl(`/blog/${post.slug}/`),
     })),
   });
 }
