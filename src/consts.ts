0c28245f0d80fc69c38a273b8fddc326ee615f46
Bryan Mey <bryan.j.mey@gmail.com>
2023-10-12 10:09:30 -0400

    add resources page. format all files

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const DONATE_URL =
   "https://donorbox.org/friends-of-lums-pond-state-park?default_interval=o";
 export const EMAIL = "friends-of-lums-pond-list@googlegroups.com";
+export const EMAIL_BOARD = "friends-of-lums-pond-board@googlegroups.com";
 export const FACEBOOK_URL = "https://www.facebook.com/FriendsofLumsPond";
 export const JOIN_URL =
   "https://docs.google.com/forms/d/e/1FAIpQLSdlFNu4wlu5ITkDM8ape6Pi6icwg4CB_ig7spxk4JjcHyHCHw/viewform?usp=sf_link";
--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,28 @@
 interface Route {
-    icon: string;
-    name: string;
-    path: string;
+  icon: string;
+  name: string;
+  path: string;
 }
 
 export const ROUTES: Route[] = [
-    {
-        icon: "fa-house",
-        name: "Home",
-        path: "",
-    },
-    {
-        icon: "fa-newspaper",
-        name: "News",
-        path: "blog",
-    },
-    {
-        icon: "fa-magnifying-glass",
-        name: "About",
-        path: "about",
-    }
+  {
+    icon: "fa-house",
+    name: "Home",
+    path: "",
+  },
+  {
+    icon: "fa-newspaper",
+    name: "News",
+    path: "blog",
+  },
+  {
+    icon: "fa-circle-info",
+    name: "About",
+    path: "about",
+  },
+  {
+    icon: "fa-book",
+    name: "Resources",
+    path: "resources",
+  },
 ];
