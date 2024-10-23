336ec876f404026ce38b78a9ca8a57c2e98aa520
Bryan Mey <bryan.j.mey@gmail.com>
2024-10-23 15:48:41 -0400

    update emails

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const DONATE_URL =
   "https://donorbox.org/friends-of-lums-pond-state-park?default_interval=o";
-export const EMAIL = "friends-of-lums-pond-list@googlegroups.com";
-export const EMAIL_BOARD = "friends-of-lums-pond-board@googlegroups.com";
+export const EMAIL = "contact@friendsoflumspond.org";
+export const EMAIL_BOARD = "executivecommittee@friendsoflumspond.org";
 export const FACEBOOK_URL = "https://www.facebook.com/FriendsofLumsPond";
 export const JOIN_URL = "https://donorbox.org/friends-of-lums-pond";
 export const SITE_DESCRIPTION = "Welcome to Friends of Lums Pond State Park!";
