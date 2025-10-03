4baf115dc65f9c7395b7c3df513ae3d3467154fc
Katie Rose <crashrose@gmail.com>
2025-10-03 17:30:51 -0400

    Added sponsors to pf-live, raffle, and faq pages

---
+++ src/pages/data/pf2025/sponsors.ts
@@ -0,0 +1,34 @@
+
+export const PLATINUM: Sponsors[] = [
+	"Christiana Care",
+	"302 Masons Lodge",
+]
+
+
+export const GOLD: Sponsors[] = [
+	"Superservice Automotive",
+	"Guild Mortgage",
+	"Easterseals"
+]
+
+export const SILVER: Sponsors[] = [
+	"Deerfield Golf Club",
+	"Bayada",
+	"Eric Morrison",
+	"Sit Means Sit",
+	"Little Bear Dog Care & Training, LLC",
+	"Academy of Dog Training"
+]
+
+export const BRONZE: Sponsors[] = [
+	"Lucky Stone",
+	"Cook's Farm",
+	"CrossFit Bear",
+	"Maldonado Landscaping"
+]
+
+export const FAMILY: Sponsors[] = [
+	"Joel Mey and Family",
+	"Jeremy Lambert and Family",
+	"The Rose Family"
+]
