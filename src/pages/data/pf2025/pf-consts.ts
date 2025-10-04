e770eee49990966502fe025988ae8341650dbc02
crashrose <crashrose@gmail.com>
2025-10-04 16:20:19 -0400

    Update pf-consts.ts

--- src/pages/data/pf2025/pf-consts.ts
+++ src/pages/data/pf2025/pf-consts.ts
@@ -1,3 +1,3 @@
-export const PF_LIVE_ALERT = "50/50 winner is 759020 with a hospital of $256! the blue ticket rqffles have been drawn and we're in the process of notifying winners."; // If no alert, leave as empty quotes
+export const PF_LIVE_ALERT = "50/50 winner is 759020 with a jackpot of $256! the blue ticket raffles have been drawn and we're in the process of notifying winners."; // If no alert, leave as empty quotes
 
 export const PF_2026_INTEREST_FORM = "https://forms.gle/dMEmKxYs7ovnyG658";
