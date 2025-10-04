963dd41c1932507fc86f408949e20b9d024b7cad
crashrose <crashrose@gmail.com>
2025-10-04 16:44:11 -0400

    Update auctions.ts

--- src/pages/data/pf2025/auctions.ts
+++ src/pages/data/pf2025/auctions.ts
@@ -10,7 +10,7 @@ export const AUCTIONS: Auction[] = [
 		id: "2025-AUC001", 
 		name: "Crochet Blanket", 
 		description: "Hand-crafted large crochet blanket, made by a member of FOLP.", 
-		status: "Winners will be announced after 4pm."
+		status: "The winner is Lynn Mey. Last 4 of phone number is 3159."
 	},
 	{
 		id: "2025-AUC002", 
@@ -70,7 +70,7 @@ export const AUCTIONS: Auction[] = [
 		id: "2025-AUC011", 
 		name: "Garrisons Lake - Foursome (1 of 2)", 
 		description: "Golf Foursome Package – Enjoy a round of golf for four at Garrisons Lake, complete with scenic fairways and a day of fun on the greens.", 
-		status: "Winners will be announced after 4pm."
+		status: "Ruby Dibecq is the winner. The last 4 of the phone number is 2453."
 	},
 	{
 		id: "2025-AUC012", 
@@ -82,66 +82,60 @@ export const AUCTIONS: Auction[] = [
 		id: "2025-AUC013", 
 		name: "Deerfield Golf Club (1 of 4)", 
 		description: "Golf Foursome Package – Enjoy a round of golf for four at Deerfield, complete with pristine fairways and an unforgettable golf experience.", 
-		status: "Winners will be announced after 4pm."
+		status: "The winner has been announced and paid."
 	},
 	{
 		id: "2025-AUC014", 
 		name: "Deerfield Golf Club (2 of 4)", 
 		description: "Golf Foursome Package – Enjoy a round of golf for four at Deerfield, complete with pristine fairways and an unforgettable golf experience.", 
-		status: "Winners will be announced after 4pm."
+		status: "The winner has been announced and paid."
 	},
 	{
 		id: "2025-AUC015", 
 		name: "Deerfield Golf Club (3 of 4)", 
 		description: "Golf Foursome Package – Enjoy a round of golf for four at Deerfield, complete with pristine fairways and an unforgettable golf experience.", 
-		status: "Winners will be announced after 4pm."
+		status: "Josh Inkell (spelling?) is the winner. Last 4 of phone number is 6217."
 	},
 	{
 		id: "2025-AUC016", 
 		name: "Deerfield Golf Club (4 of 4)", 
 		description: "Golf Foursome Package – Enjoy a round of golf for four at Deerfield, complete with pristine fairways and an unforgettable golf experience.", 
-		status: "Winners will be announced after 4pm."
+		status: "Matt Lano (spelling?) is the winner. Last 4 of phone number is 9627."
 	},
 	{
 		id: "2025-AUC017", 
 		name: "Delaware State Parks Kids Surf Fishing Vehicle", 
 		description: "Get your little angler ready for adventure! This Delaware State Parks–branded battery-powered Jeep comes with a snackle box of treats, fun park gear, and a kid-sized fishing pole—everything for the ultimate surf-fishing play prize", 
-		status: "Winners will be announced after 4pm."
+		status: "Dennis Woltemate is the winner. Last 4 of phone number is 6869."
 	},
 	{
 		id: "2025-AUC018", 
 		name: "Delaware Cornhole Set", 
 		description: "Hand-crafted Delaware themed cornhole set, made by a member of FOLP", 
-		status: "Winners will be announced after 4pm."
+		status: "The winner has been announced and paid."
 	},
 	{
 		id: "2025-AUC019", 
 		name: "USA Cornhole Set", 
 		description: "Professional grade USA themed cornhole set", 
-		status: "Winners will be announced after 4pm."
+		status: "Linda has won this auction item. The last 4 of the phone number is 2338."
 	},
 	{
 		id: "2025-AUC020", 
 		name: "1 Week Campsite Camping at Lums Pond State Park", 
 		description: "7 night stay at a campsite at LPSP, between 10/5/25 - 5/20/26 and 9/7/26 - 12/30/26. Donated by DNREC.", 
-		status: "Winners will be announced after 4pm."
-	},
-	{
-		id: "2025-AUC021", 
-		name: "Branded Patio Umbrella", 
-		description: "Donated by Cochran Liquors - Brown umbrella with Segura Viudas Cava Logo", 
-		status: "Winners will be announced after 4pm."
+		status: "Brett Francolino is the winner. Last 4 of phone number is 9212."
 	},
 	{
 		id: "2025-AUC022", 
 		name: "Amazon Mystery Box", 
 		description: "Indulge in your curiosity and find out what Amazon goods are inside of this mystery box", 
-		status: "Winners will be announced after 4pm."
+		status: "Jen Clemmons is the winner. Last 4 of phone number is 0975."
 	},
 	{
 		id: "2025-AUC023", 
 		name: "Pond Fest Wall Art", 
 		description: "Hand-crafted Pond Fest wall art, made by a member of FOLP", 
-		status: "Winners will be announced after 4pm."
+		status: "Bob is the winner. The last 4 of phone number is 2337."
 	},
 ];
