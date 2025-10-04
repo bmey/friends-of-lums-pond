f36ec68352ac6b7d2be6932fbca8232bca51118d
Bryan Mey <bryan.j.mey@gmail.com>
2025-10-03 22:22:48 -0400

    sponsor names

--- src/pages/data/pf2025/sponsors.ts
+++ src/pages/data/pf2025/sponsors.ts
@@ -1,34 +1,32 @@
-
 export const PLATINUM: Sponsors[] = [
-	"Christiana Care",
-	"302 Masons Lodge",
-]
-
+  "Christiana Care",
+  "Granite-Corinthian Lodge No. 34 A.F. & A.M.",
+];
 
 export const GOLD: Sponsors[] = [
-	"Superservice Automotive",
-	"Guild Mortgage",
-	"Easterseals"
-]
+  "Superservice Automotive",
+  "MOT Lending Group at Guild Mortgage",
+  "Easterseals Delaware & Maryland's Eastern Shore",
+];
 
 export const SILVER: Sponsors[] = [
-	"Deerfield Golf Club",
-	"Bayada",
-	"Eric Morrison",
-	"Sit Means Sit",
-	"Little Bear Dog Care & Training, LLC",
-	"Academy of Dog Training"
-]
+  "Deerfield Golf Club",
+  "Bayada Pediatrics",
+  "Eric Morrison",
+  "Sit Means Sit",
+  "Little Bear Dog Care & Training, LLC",
+  "Academy of Dog Training & Agility",
+];
 
 export const BRONZE: Sponsors[] = [
-	"Lucky Stone",
-	"Cook's Farm",
-	"CrossFit Bear",
-	"Maldonado Landscaping"
-]
+  "Lucky Stone",
+  "Cook Family Farms",
+  "CrossFit Bear",
+  "Maldonado Landscaping",
+];
 
 export const FAMILY: Sponsors[] = [
-	"Joel Mey and Family",
-	"Jeremy Lambert and Family",
-	"The Rose Family"
-]
+  "Joel Mey and Family",
+  "Jeremy Lambert and Family",
+  "The Rose Family",
+];
