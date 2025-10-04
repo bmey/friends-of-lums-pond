c2711380b3cd03c057fb7cc81c61f4a98040975a
Katie Rose <crashrose@gmail.com>
2025-10-04 01:30:10 -0400

    Update raffles, auction, and beer details

--- src/pages/data/pf2025/raffles.ts
+++ src/pages/data/pf2025/raffles.ts
@@ -6,187 +6,184 @@ interface Raffle {
 }
 
 export const RAFFLES: Raffle[] = [
-    {
-    id: "raf001", 
-    name: "Branded Patio Umbrella", 
-    description: "Brown patio umbrella with sparkling wine logo: Segura Viudas CAVA -  Donated by Cochran Liquors", 
-    status: "Drawing will be held at 3pm at Nest Box"
-  },
-
   {
-    id: "raf002", 
+    id: "2025-RAF002", 
     name: "Your Creation Station Basket", 
     description: "$50 gift card and mini paint kid from your creation station - YCS will fire your painted mini kit for free!", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf003", 
+    id: "2025-RAF003", 
     name: "CrossFit Bear Month Membership", 
     description: "One month membership to CrossFit Bear", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf004", 
-    name: "Bird House Basket", 
+    id: "2025-RAF004", 
+    name: "Bird House Basket (1 of 2)", 
     description: "Hand crafted bird house by a member of the FOLP", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf005", 
-    name: "Bird House Basket", 
+    id: "2025-RAF005", 
+    name: "Bird House Basket (2 of 2)", 
     description: "Hand crafted bird house by a member of the FOLP", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf007", 
-    name: "REI Gift Basket", 
+    id: "2025-RAF006", 
+    name: "Fall Goodies Basket", 
+    description: "Fall Goodies Basket: cookies, candy, camping book, pumpkin bread mix, little mums, s'mores socks, ceramic pumpkin, candle", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
+  {
+    id: "2025-RAF007", 
+    name: "REI Gift Basket (1 of 2)", 
     description: "REI hydration vest (size M/L), REI tumbler, quick dry towel. Donated by REI.", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf008", 
-    name: "REI Gift Basket", 
+    id: "2025-RAF008", 
+    name: "REI Gift Basket (2 of 2)", 
     description: "REI hydration vest (size L/XL), REI tumbler, quick dry towel. Donated by REI.", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf009", 
+    id: "2025-RAF009", 
     name: "Winter Dog Basket 21+ (whiskey)", 
     description: "Womens winter mittens, fleece blanket, dog bandana, dog collar, dog winter jacket, big jac treats, dog toy, 3 small bottles bird dog whiskey", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf010", 
+    id: "2025-RAF010", 
     name: "Bath & Wine Basket 21+ (wine)", 
     description: "Vanilla almond spa bath set, MAN Chenin Blanc Wine, Propietary Red Wine Blend Z. Alexander", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf011", 
+    id: "2025-RAF011", 
     name: "Cat Basket", 
     description: "Cat toys, games, foods and treats.", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf012", 
-    name: "Pet basket", 
+    id: "2025-RAF012", 
+    name: "Pet basket from Companion Animal Veterinary Association", 
     description: "Dog toys, treats, towels, Halloween tumbler, candle donated by Companion Animal Veterinary Association", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf013", 
-    name: "Local Author Gift Basket", 
-    description: "Coloring books, plush animals, coloring supplies and toys, and story books by local author Mark Collins. Donated by Mark Collins.", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    id: "2025-RAF013", 
+    name: "Mark Collins, Local Author Gift Basket", 
+    description: "Coloring books, plush animals, coloring supplies and toys, and story books by loca author Mark Collins. Donated by Mark Collins.", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf014", 
+    id: "2025-RAF014", 
     name: "Little Bear Dog Care Basket", 
-    description: "$50 off a service coupon, stuffed animal, chews, treats, beef knuckle, keychain, zipper pull, bandana, poop bags", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    description: "$50 off a service coupon, crocheted dog print blanket, stuffed animal, chews, treats, beef knuckle, keychain, zipper pull, bandana, poop bangs", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf015", 
-    name: "Crooked Hammock Brewing Basket", 
-    description: "$35 gift card, 2 four packs, candle, kitchen towels, fall decorations donated by Crooked Hammock", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    id: "2025-RAF015", 
+    name: "Crooked Hammock Gift Basket (21+)", 
+    description: "$35 gift card, 2 four packs, candle, kitchen towels, fall decorations ", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf016", 
+    id: "2025-RAF016", 
     name: "Nature Gift Basket ", 
     description: "Bird House Lego Set and crotched Bee, Donated by Girlscout Troop 122", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf017", 
+    id: "2025-RAF017", 
     name: "Kids Disc Golf Set", 
     description: "Kids Disc Golf Set", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf018", 
+    id: "2025-RAF018", 
     name: "Fort Delaware Park Passes ", 
-    description: "Two Delaware VIP Visitor Passes to Fort Delaware. Donated by DNREC", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    description: "Two Delaware VIP Visitor Pass. Donated by DNREC", 
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf019", 
-    name: "Stewart's Brewing Gift Card", 
+    id: "2025-RAF019", 
+    name: "Stewart's Brewing Company Gift Card", 
     description: "Gift Card to Stewart's Brewing Co. $50", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf020", 
+    id: "2025-RAF020", 
     name: "New Look Nail Salon Gift Card", 
     description: "Gift card to New Look Nail Salon for one pedicure", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf021", 
-    name: "Lucky stone Basket", 
-    description: "Lucky Stone L/XL shirt, guitar wall mount, Elixir electric guitar strings, clover earrings ", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
-  {
-    id: "raf022", 
-    name: "Willey's Farm Gift Card", 
-    description: " $20 Gift card  and festive mum plant donated by Willey Farm", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    id: "2025-RAF021", 
+    name: "Lucky Stone Basket", 
+    description: "Lucky Stone L/XL shirt, guitar wall mount, Elixir electric guitar strings, shamrock earrings ", 
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf023", 
+    id: "2025-RAF022", 
+    name: "Willey Farms Gift Card and Mum", 
+    description: "$20 gift card to Willey Farms and festive mum plant", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
+  {
+    id: "2025-RAF023", 
     name: "Cadillac Gift Basket", 
     description: "Branded shirts, koozie, tumbler, keychains, lock, reusable bags, jewelry and more", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf024", 
+    id: "2025-RAF024", 
     name: "Jaguar Gift Basket", 
     description: "Branded hats, tumbler, car accessories, bracelets", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf025", 
+    id: "2025-RAF025", 
     name: "Bone-Appetit Dog Gift Basket", 
-    description: "MilkBone dog treats, biscuits, chews, and magnet donated by Bone-Appetit", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    description: "MilkBone dog treats, biscuits, chews, and magnet", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf026", 
+    id: "2025-RAF026", 
     name: "Coffee Gift Basket", 
     description: "Dunkin' medium roast, cafe espresso dark roast, Black Gold ground coffee, Folgers gourmet ground coffee, Folgers french vanilla instant coffee", 
-    status: "Drawing will be held at 3pm at Nest Box"},
-
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
   {
-    id: "raf027", 
+    id: "2025-RAF027", 
     name: "Compass Wall Art", 
     description: "Hand-crafted compass wall art, made by a member of FOLP", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
-
   {
-    id: "raf028", 
+    id: "2025-RAF028", 
     name: "Pineapple \"Welcome\" Wall Art", 
     description: "Hand-crafted pineapple wall art, made by a member of FOLP", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    status: "Drawing will be held at 3pm at Nest Box."
   },
   {
-    id: "raf029", 
-    name: "Pond Fest Basket", 
-    description: "Membership coupon, 2 Pond Fest koozies, 2 Pond Fest stickers, 1 Large blue event shirt. Donated by FOLP.", 
-    status: "Drawing will be held at 3pm at Nest Box"
+    id: "2025-RAF029", 
+    name: "FOLP Pond Fest Gift Basket", 
+    description: "FOLP membership coupon, 2 PF koozies, 2 PF stickers, 1 Large blue event shirt. Donated by FOLP.", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
+  {
+    id: "2025-RAF030", 
+    name: "Elf Pets Puppy and Playset", 
+    description: "St Bernard plush, illustrated storybook, accessories", 
+    status: "Drawing will be held at 3pm at Nest Box."
+  },
+  {
+    id: "2025-RAF031", 
+    name: "Dalmatian Toy Vet Kit", 
+    description: "Dalmatian Toy Vet Kit", 
+    status: "Drawing will be held at 3pm at Nest Box."
   },
 ];
