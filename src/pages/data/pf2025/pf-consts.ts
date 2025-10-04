024c4f7dffd0abcc14f97d62c804007181786353
crashrose <crashrose@gmail.com>
2025-10-04 16:07:06 -0400

    Update pf-consts.ts

--- src/pages/data/pf2025/pf-consts.ts
+++ src/pages/data/pf2025/pf-consts.ts
@@ -1,3 +1,3 @@
-export const PF_LIVE_ALERT = "Raffles will be drawn at 3pm."; // If no alert, leave as empty quotes
+export const PF_LIVE_ALERT = "50/50 winner is 759020! the blue ticket takes have been drawn and we're in the process of notifying winners."; // If no alert, leave as empty quotes
 
 export const PF_2026_INTEREST_FORM = "https://forms.gle/dMEmKxYs7ovnyG658";
